4b1893ed4536c00d7aebf7155e285dc7e406e1df
Suhaib Saeed <saeed.suhaib@googlemail.com>
2021-02-01 17:48:37 +0000

    added animals function for return of individual pages

--- Introduction to Flask/adopt_a_pet_project.py
+++ Introduction to Flask/adopt_a_pet_project.py
@@ -8,9 +8,25 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   """
-  Return HTML element with text 'Adopt a pet'
+  Return HTML element with text 'Adopt a pet', paragraph and an unordered list of animals
   """
   return """
   <h1> Adopt a pet </h1>
   <p> Browse through the links below to find your new furry friend </p>
+  <ul>
+    <li>Dogs</li>
+    <li>Cats</li>
+    <li>Rabbits</li>
+  </ul>
   """
+
+# Function which will return individual pages for animal types and link them in the list
+# Use route decorator function to bind '/animals/pet_type' URL path to the function
+@app.route('/animals/<string:pet_type>')
+def animals(pet_type):
+  """
+  Return HTML element with header 'List of pets'
+  """
+  html = "<h1> List of pet </h1>"
+
+  return html
