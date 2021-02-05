c9583db8739eeb2cb628620b4abf63ec5208c795
Suhaib Saeed <saeed.suhaib@googlemail.com>
2021-02-05 00:01:29 +0000

    Completed up to section 13

--- Introduction to Flask/adopt_a_pet_project.py
+++ Introduction to Flask/adopt_a_pet_project.py
@@ -1,32 +1,45 @@
 from flask import Flask
+from helper import pets
 
 # Create instance of Flask class and save as object called app
 app = Flask(__name__)
+
 
 # Create index route via function
 # Use route decorator function to bind '/' URL path to the function
 @app.route('/')
 def index():
-  """
+    """
   Return HTML element with text 'Adopt a pet', paragraph and an unordered list of animals
   """
-  return """
+    return """
   <h1> Adopt a pet </h1>
   <p> Browse through the links below to find your new furry friend </p>
   <ul>
-    <li>Dogs</li>
-    <li>Cats</li>
-    <li>Rabbits</li>
+    <li><a href= '/animals/dogs'>Dogs</a></li>
+    <li><a href= '/animals/cats'>Cats</a></li>
+    <li> <a href= '/animals/rabbits'>Rabbits</a></li>
   </ul>
   """
+
 
 # Function which will return individual pages for animal types and link them in the list
 # Use route decorator function to bind '/animals/pet_type' URL path to the function
 @app.route('/animals/<string:pet_type>')
 def animals(pet_type):
-  """
+    """
   Return HTML element with header 'List of pets'
   """
-  html = "<h1> List of pet </h1>"
+    html = f"<h1> List of {pet_type}</h1>"
+    html += "<ul>"
+    for i in pets[pet_type]:
+        new_html = f"<li> {i['name']} </li>"
+        html += new_html
+    html += "</ul>"
 
-  return html
+    return html
+
+
+@app.route('/animals/<string:pet_type>/<int:pet_id>')
+def pet(pet_id):
+  pass
