2868a784274d7e7ed64d3df1441cdcc6d8e55881
Suhaib Saeed <saeed.suhaib@googlemail.com>
2021-02-06 22:34:20 +0000

    Finished project and added file with pets dictionary

--- Introduction to Flask/adopt_a_pet_project.py
+++ Introduction to Flask/adopt_a_pet_project.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """
-  Return HTML element with text 'Adopt a pet', paragraph and an unordered list of animals
-  """
+    Return HTML element with text 'Adopt a pet', paragraph and an unordered list of animals
+    """
     return """
   <h1> Adopt a pet </h1>
   <p> Browse through the links below to find your new furry friend </p>
@@ -22,24 +22,39 @@ def index():
   </ul>
   """
 
-
 # Function which will return individual pages for animal types and link them in the list
 # Use route decorator function to bind '/animals/pet_type' URL path to the function
 @app.route('/animals/<string:pet_type>')
 def animals(pet_type):
     """
-  Return HTML element with header 'List of pets'
-  """
+    Return HTML element with unordered list of each pet type
+    """
     html = f"<h1> List of {pet_type}</h1>"
     html += "<ul>"
-    for i in pets[pet_type]:
-        new_html = f"<li> {i['name']} </li>"
+    # Loop through list of pet types and create links for them based on their ID
+    for count, i in enumerate(pets[pet_type]):
+        new_html = f"<li><a href= /animals/{pet_type}/{count}>{i['name']}</a></li>"
         html += new_html
     html += "</ul>"
 
     return html
 
 
+# Create and link individual profile pages for each pet
 @app.route('/animals/<string:pet_type>/<int:pet_id>')
-def pet(pet_id):
-  pass
+def pet(pet_type, pet_id):
+    """
+    Return HTML element with the pets name, image, description and unordered list with breed and age
+    """
+    # Get pet dictionary from the pets dictionary
+    pet = pets[pet_type][pet_id]
+
+    return f"""
+  <h1> {pet['name']} </h1>
+  <img src={pet['url']} />
+  <p> {pet['description']} </p>
+   <ul>
+    <li>Breed: {pet['breed']}</li>
+    <li>Age: {pet['age']}</li>
+  </ul>
+  """
