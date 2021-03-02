2991fcfeff226577af400d11f4f13260a4495f2c
Suhaib Saeed <saeed.suhaib@googlemail.com>
2021-03-02 22:53:20 +0000

    Added code to extract data from form and index html doc

--- Flask Forms/app.py
+++ Flask Forms/app.py
@@ -3,31 +3,27 @@ from helper import recipes, descriptions, ingredients, instructions, add_ingredi
 
 app = Flask(__name__)
 
-
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # Variable used as key for new recipe data
-    new_id = len(recipes) + 1
-    # Makes sure there is data in form before trying to access them
-    if len(request.form) > 0:
-        #### Add the recipe name to recipes[new_id]
-
-        #### Add the recipe description to descriptions[new_id]
-
-        #### Add the values to new_ingredients and new_instructions
-        new_ingredients = None
-        new_instructions = None
-        add_ingredients(new_id, new_ingredients)
-        add_instructions(new_id, new_instructions)
-    return render_template("index.html", template_recipes=recipes)
-
+  # Variable used as key for new recipe data
+  new_id = len(recipes) + 1
+  # Makes sure there is data in form before trying to access them
+  if len(request.form) > 0:
+    #### Add the recipe name to recipes[new_id]
+    recipes[new_id] = request.form["recipe"]
+    #### Add the recipe description to descriptions[new_id]
+    descriptions[new_id] = request.form["description"]
+    #### Add the values to new_ingredients and new_instructions
+    new_ingredients = request.form["ingredients"]
+    new_instructions = request.form["instructions"]
+    add_ingredients(new_id, new_ingredients)
+    add_instructions(new_id, new_instructions)
+  return render_template("index.html", template_recipes=recipes)
 
 @app.route("/recipe/<int:id>")
 def recipe(id):
-    return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id],
-                           template_ingredients=ingredients[id], template_instructions=instructions[id])
-
+  return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id], template_ingredients=ingredients[id], template_instructions=instructions[id])
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+  return render_template("about.html")
