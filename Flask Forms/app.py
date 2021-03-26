43d8eae80939cd093176d912604af03d70e10c4a
Suhaib Saeed <63662275+suhaibasaeed@users.noreply.github.com>
2021-03-26 10:19:15 +0000

    section 5 - Handling flask form data

--- Flask Forms/app.py
+++ Flask Forms/app.py
@@ -6,12 +6,9 @@ from wtforms import StringField, SubmitField
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mysecret"
 
-#### Create form class here
 class CommentForm(FlaskForm):
-  # Class attributes
-  comment = StringField("Comment")
+  comment =  StringField("Comment")
   submit = SubmitField("Add Comment")
-
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -19,8 +16,12 @@ def index():
 
 @app.route("/recipe/<int:id>", methods=["GET", "POST"])
 def recipe(id):
-  #### Instantiate form class here
   comment_form = CommentForm()
+  #### Process data here
+  # Access comment data field via attribute
+  new_comment = comment_form.comment.data
+  # Append the comment to the comments[id] list
+  comments[id].append(new_comment)
 
   return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id], template_ingredients=ingredients[id], template_instructions=instructions[id], template_comments=comments[id], template_form=comment_form)
 
