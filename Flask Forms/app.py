da670d2987dbcee0f74ff3933df4c735be1252e2
Suhaib Saeed <63662275+suhaibasaeed@users.noreply.github.com>
2021-03-26 10:44:50 +0000

    section 7 - validation

--- Flask Forms/app.py
+++ Flask Forms/app.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template, request
 from helper import recipes, descriptions, ingredients, instructions, add_ingredients, add_instructions, comments
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+#### Note the new import statement
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "mysecret"
 
 class CommentForm(FlaskForm):
-  comment =  StringField("Comment")
+  #### Add a validator argument in the StringField
+  comment =  StringField("Comment", validators=[DataRequired()])
   submit = SubmitField("Add Comment")
 
 @app.route("/", methods=["GET", "POST"])
@@ -16,13 +19,11 @@ def index():
 
 @app.route("/recipe/<int:id>", methods=["GET", "POST"])
 def recipe(id):
-  comment_form = CommentForm()
-  ### Process data here
-  # Access comment data field via attribute
-  new_comment = comment_form.comment.data
-  # Append the comment to the comments[id] list
-  comments[id].append(new_comment)
-
+  comment_form = CommentForm(csrf_enabled=False)
+  #### Replace 'True' with form validation
+  if comment_form.validate_on_submit():
+    new_comment = comment_form.comment.data
+    comments[id].append(new_comment)
   return render_template("recipe.html", template_recipe=recipes[id], template_description=descriptions[id], template_ingredients=ingredients[id], template_instructions=instructions[id], template_comments=comments[id], template_form=comment_form)
 
 @app.route("/about")
