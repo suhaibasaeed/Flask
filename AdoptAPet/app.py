dd4740c3c516e632ed59e66fe84146f614ee2f14
Suhaib Saeed <saeed.suhaib@googlemail.com>
2023-07-05 20:56:56 +0100

    Added animals route

--- AdoptAPet/app.py
+++ AdoptAPet/app.py
@@ -9,10 +9,18 @@ def index():
     <h1>Adopt a Pet!</h1>
     <p>Browse through the links below to find your new furry friend:</p>
     <ul>
-        <li>Dogs</li>
-        <li>Cats</li>
-        <li>Rabbits</li>
+        <li><a href='/animals/dogs'>Dogs</a></li>
+        <li><a href='/animals/cats'>Cats</a></li>
+        <li><a href='/animals/rabbits'>Rabbits</a></li>
     </ul>
     """
+
+@app.route('/animals/<pet_type>')   
+def animals(pet_type):
+
+    html = f"<h1>List of {pet_type}</h1>"
+
+    return html
+
 
 app.run(debug=True, port=8000)
